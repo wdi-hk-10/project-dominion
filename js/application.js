0fb86b7a62f497c43d6799a80b6c9a17a26c08c9
Judah Frankel <judahfrankel@gmail.com>
2016-01-26 15:09:11 +0800

    About to start basic testing.

--- js/application.js
+++ js/application.js
@@ -57,154 +57,180 @@ $(document).ready(function(){
 });
 
 var cards = {
-    "Copper": {
-      "supply": 60,
-      "cost": 0,
-      "type": {
-        // I wrote it like this after doing some reading on JS Objects
-        "Treasure": true
-      },
-      "treasureValue": 1
+  "Copper": {
+    "supply": 60,
+    "cost": 0,
+    "type": {
+      // I wrote it like this after doing some reading on JS Objects
+      "Treasure": true
     },
-    "Silver": {
-      "supply": 40,
-      "cost": 3,
-      "type": {
-        "Treasure": true
-      },
-      "treasureValue": 2
-    }
-    "Gold": {
-      "supply": 30,
-      "cost": 6,
-      "type": {
-        "Treasure": true
-      },
-      "treasureValue": 3
-    }
-    "Estate": {
-      "supply": 8,
-      "cost": 2,
-      "type": {
-        "Victory": true
-      },
-      "victoryValue": 1
-    }
-    "Duchy": {
-      "supply": 8,
-      "cost": 5,
-      "type": {
-        "Victory": true
-      },
-      "victoryValue": 3
-    }
-    "Province": {
-      "supply": 8,
-      "cost": 8,
-      "type": {
-        "Victory": true
-      },
-      "victoryValue": 6
-    }
-    "Cellar": {
-      "supply": 10,
-      "cost": 2,
-      "type": {
-        "Action": true
-      },
-      "effects": {
+    "treasureValue": 1
+  },
+  "Silver": {
+    "supply": 40,
+    "cost": 3,
+    "type": {
+      "Treasure": true
+    },
+    "treasureValue": 2
+  }// ,
+  // "Gold": {
+  //   "supply": 30,
+  //   "cost": 6,
+  //   "type": {
+  //     "Treasure": true
+  //   },
+  //   "treasureValue": 3
+  // },
+  // "Estate": {
+  //   "supply": 8,
+  //   "cost": 2,
+  //   "type": {
+  //     "Victory": true
+  //   },
+  //   "victoryValue": 1
+  // },
+  // "Duchy": {
+  //   "supply": 8,
+  //   "cost": 5,
+  //   "type": {
+  //     "Victory": true
+  //   },
+  //   "victoryValue": 3
+  // },
+  // "Province": {
+  //   "supply": 8,
+  //   "cost": 8,
+  //   "type": {
+  //     "Victory": true
+  //   },
+  //   "victoryValue": 6
+  // },
+  // "Cellar": {
+  //   "supply": 10,
+  //   "cost": 2,
+  //   "type": {
+  //     "Action": true
+  //   },
+  //   "effects": [
+  //     {
+  //       "name": "plusAction",
+  //       "amount": 1
+  //     },
+  //     {
+  //       "name": "draw/discard",
+  //       "": drawDiscard
+  //     }
+  //   ]
+  // },
+  // cards["Cellar"].effects[1].xxx()
+  // "Market": {
+  //   "supply": 10,
+  //   "cost": 5,
+  //   "type": {
+  //     "Action": true
+  //   },
+  //   "effects": {
 
-      }
-    }
-    "Market": {
-      "supply": 10,
-      "cost": 5,
-      "type": {
-        "Action": true
-      },
-      "effects": {
+  //   }
+  // },
+  // "Mine": {
+  //   "supply": 10,
+  //   "cost": 5,
+  //   "type": {
+  //     "Action": true
+  //   },
+  //   "effects": {
 
-      }
-    }
-    "Mine": {
-      "supply": 10,
-      "cost": 5,
-      "type": {
-        "Action": true
-      },
-      "effects": {
+  //   }
+  // },
+  // "Remodel": {
+  //   "supply": 10,
+  //   "cost": 4,
+  //   "type": {
+  //     "Action": true
+  //   },
+  //   "effects": {
 
-      }
-    }
-    "Remodel": {
-      "supply": 10,
-      "cost": 4,
-      "type": {
-        "Action": true
-      },
-      "effects": {
+  //   }
+  // },
+  // "Smithy": {
+  //   "supply": 10,
+  //   "cost": 4,
+  //   "type": {
+  //     "Action": true
+  //   },
+  //   "effects": {
 
-      }
-    }
-    "Smithy": {
-      "supply": 10,
-      "cost": 4,
-      "type": {
-        "Action": true
-      },
-      "effects": {
+  //   }
+  // },
+  // "Village": {
+  //   "supply": 10,
+  //   "cost": 3,
+  //   "type": {
+  //     "Action": true
+  //   },
+  //   "effects": {
 
-      }
-    }
-    "Village": {
-      "supply": 10,
-      "cost": 3,
-      "type": {
-        "Action": true
-      },
-      "effects": {
+  //   }
+  // },
+  // "Woodcutter": {
+  //   "supply": 10,
+  //   "cost": 3,
+  //   "type": {
+  //     "Action": true
+  //   },
+  //   "effects": {
 
-      }
-    }
-    "Woodcutter": {
-      "supply": 10,
-      "cost": 3,
-      "type": {
-        "Action": true
-      },
-      "effects": {
+  //   }
+  // },
+  // "Workshop": {
+  //   "supply": 10,
+  //   "cost": 3,
+  //   "type": {
+  //     "Action": true
+  //   },
+  //   "effects": {
 
-      }
-    }
-    "Workshop": {
-      "supply": 10,
-      "cost": 3,
-      "type": {
-        "Action": true
-      },
-      "effects": {
-
-      }
-    }
-    "Council Room": {
-      "supply": 10,
-      "cost": 5,
-      "type": {
-        "Action": true
-      },
-      "effects": {
-
-      }
-    }
-    "Laboratory": {
-      "supply": 10,
-      "cost": 5,
-      "type": {
-        "Action": true
-      },
-      "effects": {
-
-      }
-    }
+  //   }
+  // },
+  // "Council Room": {
+  //   "supply": 10,
+  //   "cost": 5,
+  //   "type": {
+  //     "Action": true
+  //   },
+  //   "effects": [
+  //     {
+  //       "name": "plusCard",
+  //       "amount": 4
+  //     },
+  //     {
+  //       "name": "plusBuy",
+  //       "amount": 1
+  //     },
+  //     {
+  //       "name": "otherPlayersDraw",
+  //       "amount": 1
+  //     }
+  //   ]
+  // },
+  // "Laboratory": {
+  //   "supply": 10,
+  //   "cost": 5,
+  //   "type": {
+  //     "Action": true
+  //   },
+  //   "effects": [
+  //     {
+  //       "name": "plusCard",
+  //       "amount": 2
+  //     },
+  //     {
+  //       "name": "plusAction",
+  //       "amount": 1
+  //     }
+  //   ]
+  // }
 }
 
